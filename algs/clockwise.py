5fbc837558a029ae4aae7d9e80881d5bcee03787
Aubrey <157611466+aconners@users.noreply.github.com>
2024-04-21 19:21:24 -0400

    Add files via upload

--- algs/clockwise.py
+++ algs/clockwise.py
@@ -1,26 +1,103 @@
-class Clockwise():
-    
-    game = None
+class PacMan:
+    def __init__(self, maze):
+        self.maze = maze
+        self.position = self.find_start_position()
+        self.direction = 'right'
+        self.game_over = False
+        self.game = None  # Variable called game initialized to None
+
+    def find_start_position(self):
+        # Find Pac-Man's starting position in the maze
+        for i in range(len(self.maze)):
+            for j in range(len(self.maze[0])):
+                if self.maze[i][j] == 'P':
+                    return i, j
+        return None
+
+    def can_move(self, x, y):
+        # Check if Pac-Man can move to the given position
+        if 0 <= x < len(self.maze) and 0 <= y < len(self.maze[0]):
+            return self.maze[x][y] != '#'
+        return False
+
+    def move(self):
+        # Move Pac-Man according to the clockwise algorithm
+        if self.game_over:
+            return False
+
+        x, y = self.position
+        if self.direction == 'right':
+            if self.can_move(x, y + 1):
+                self.position = (x, y + 1)
+                self.direction = 'down'
+            elif self.can_move(x + 1, y):
+                self.position = (x + 1, y)
+                self.direction = 'right'
+            elif self.can_move(x, y - 1):
+                self.position = (x, y - 1)
+                self.direction = 'up'
+            else:
+                self.game_over = True
+        elif self.direction == 'down':
+            if self.can_move(x + 1, y):
+                self.position = (x + 1, y)
+                self.direction = 'left'
+            elif self.can_move(x, y - 1):
+                self.position = (x, y - 1)
+                self.direction = 'up'
+            elif self.can_move(x - 1, y):
+                self.position = (x - 1, y)
+                self.direction = 'right'
+            else:
+                self.game_over = True
+        elif self.direction == 'left':
+            if self.can_move(x, y - 1):
+                self.position = (x, y - 1)
+                self.direction = 'up'
+            elif self.can_move(x - 1, y):
+                self.position = (x - 1, y)
+                self.direction = 'left'
+            elif self.can_move(x, y + 1):
+                self.position = (x, y + 1)
+                self.direction = 'down'
+            else:
+                self.game_over = True
+        elif self.direction == 'up':
+            if self.can_move(x - 1, y):
+                self.position = (x - 1, y)
+                self.direction = 'right'
+            elif self.can_move(x, y + 1):
+                self.position = (x, y + 1)
+                self.direction = 'down'
+            elif self.can_move(x + 1, y):
+                self.position = (x + 1, y)
+                self.direction = 'left'
+            else:
+                self.game_over = True
+        return not self.game_over
 
     def setup(self):
+        # Placeholder for setup method, no calculations needed
         pass
-    
+
     def get_dir(self):
-        poss_moves = self.game.pacman.allowed_moves
-        current_dir = self.game.pacman.direction
-        clockwise_dirs = ["up", "right", "down", "left"]
-        for direction in clockwise_dirs:
-            next_dir_index = (clockwise_dirs[current_dir + 1] % 4) 
-            #%4 will make it so "left" + 1 points to "up" instead of throwing an error
-            next_dir = clockwise_dirs[next_dir_index]
-            if direction == current_dir and next_dir in poss_moves:
-                current_dir = next_dir
-            else:
-                continue
-        return current_dir
-        #Need to make code that makes sure p
+        return self.direction  # Method to return the current direction
 
-                
+# Example usage:
+maze = [
+    ['#', '#', '#', '#', '#'],
+    ['#', '.', '.', '.', '#'],
+    ['#', '.', 'P', '.', '#'],
+    ['#', '.', '.', '.', '#'],
+    ['#', '#', '#', '#', '#']
+]
 
-    
-            
+pacman = PacMan(maze)
+
+pacman.setup()  # Call setup method before starting the game
+
+while pacman.move():
+    print("Pac-Man's current position:", pacman.position)
+
+if pacman.game_over:
+    print("Game over! Pac-Man cannot move further.")
